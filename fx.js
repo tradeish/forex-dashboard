0570d0753712df06d7086217b7669a589d60402e
tradeish <69721696+tradeish@users.noreply.github.com>
2026-08-01 15:05:08 +0600

    Update fx.js

--- fx.js
+++ fx.js
@@ -167,9 +167,9 @@ function fxShowRelated(pair, entry){
       img.alt = title;
       img.loading = "lazy";
       img.style.setProperty("width","100px","important");
-      img.style.setProperty("height","90px","important");
+      img.style.setProperty("height","80px","important");
       img.style.setProperty("max-width","100px","important");
-      img.style.setProperty("max-height","90px","important");
+      img.style.setProperty("max-height","80px","important");
       img.style.setProperty("object-fit","contain","important");
       img.style.setProperty("border-radius","5px","important");
       imgEl.parentNode.replaceChild(img, imgEl);
